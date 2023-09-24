3e7fd5322da61696a510c37699c45d3cd24bdfdc
Julious <niloyjulious@gmail.com>
2023-09-25 00:34:51 +0600

    made the interface and added some logic

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-function operate(operator, num1, num2){
-    switch (operator){
-        case '+' : return (num1 + num2);
-        case '-' : return (num1 - num2);
-        case '*' : return (num1 * num2);
-        case '/' : return (num1 / num2);
+let operator = "";
+let num1 = 0;
+let num2 = 0;
+
+
+const screen01 = document.getElementById("screen01");
+screen01.textContent = " ";
+
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+': return (num1 + num2);
+        case '-': return (num1 - num2);
+        case '*': return (num1 * num2);
+        case '/': return (num1 / num2);
     }
 }
 
 // const total = calculate("-",34,2);
 // console.log("🚀 ~ file: script.js:11 ~ total:", total)
 
-console.log("asdasd");
+// console.log("asdasd");
 
 
-let operator = "";
-let num1 = 0;
-let num2 = 0;
 
-const screen01 = document.querySelector(id="screen01");
+
+
 
 const gridContainer = document.querySelector('.gridContainer')
 
@@ -26,7 +33,7 @@ const buttons = gridContainer.querySelectorAll('.grid-item');
 buttons.forEach(button => button.addEventListener('click', updateScreen));
 
 
-function updateScreen(e){
+function updateScreen(e) {
     const screen01 = document.getElementById("screen01");
     screen01.textContent = screen01.textContent + e.target.textContent;
 }
